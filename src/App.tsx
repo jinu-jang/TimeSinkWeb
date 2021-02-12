f709ce4a23e93a9ae3f063385ba0b2eb9cdbc948
jinu-jang <jimjang95@gmail.com>
2021-02-12 06:32:14 -0800

    Infinte loop created. (#1)
    Ouroboros Component created to implement carousel style scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from './Header';
+import OuroborosPage from './components/OuroborosPage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OuroborosPage backgroundColors={['DodgerBlue', 'red', '#99CC33', 'coral']}/>
     </div>
   );
 }
