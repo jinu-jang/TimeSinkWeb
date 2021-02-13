73ad29ec641ae96ca3d32f2e3c49a250d21857b1
Jinu Jang <jimjang95@gmail.com>
2021-02-13 04:03:54 -0800

    Adding horizontal animation to first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import FullPageBackground from './components/FullPageBackground';
 import OuroborosPage from './components/OuroborosPage';
 import FirstTestPage from './components/FirstTestPage';
 
 function App() {
-  let children = ['DodgerBlue', 'red', '#99CC33', 'coral'].map((color) => {
+  // let children = ['DodgerBlue', 'red', '#99CC33', 'coral'].map((color) => {
+  //   return <FullPageBackground backgroundColor={color} />
+  // });
+  let children = ['#1E4174', '#1E4174', '#1E4174', '#1E4174'].map((color) => {
     return <FullPageBackground backgroundColor={color} />
   });
   children.unshift(<FirstTestPage/>);
   return (
-    <div className="App">
-      <OuroborosPage >
-        {children}
-      </OuroborosPage>
-    </div>
+    <ParallaxProvider>
+      <div className="App">
+        <OuroborosPage >
+          {children}
+        </OuroborosPage>
+      </div>
+    </ParallaxProvider>
   );
 }
 
--- src/components/FirstTestPage.tsx
+++ src/components/FirstTestPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Parallax } from 'react-scroll-parallax'
 
 interface IProps {
 }
@@ -17,8 +18,21 @@ const Background = styled.div<IProps>`
 const Title = styled.h1`
     font-size: 10em;
     text-align: center;
-    color: #DDA94B;
+    text-shadow: 5px 3px 0 #DDA94B;
+    color: #103A6E;
+    margin: 0;
 `;
+
+const Horizontal = styled.h1`
+    font-size: 10em;
+    text-align: center;
+    text-shadow: 5px 3px 0 #DDA94B;
+    color: #103A6E;
+    margin: 0;
+    position: absolute;
+    top: 40%;
+    left: 40%;
+`
 
 export default class FullPageBackground extends React.Component<IProps, IState> {
 
@@ -28,6 +42,12 @@ export default class FullPageBackground extends React.Component<IProps, IState> 
                 <Title>
                     Hello!
                 </Title>
+                <Parallax
+                    y={[-70, 70]}
+                    x={[30, -30]}
+                >
+                    <Horizontal className="h1">Horizontal</Horizontal>
+                </Parallax>
             </Background>
         );
     }
